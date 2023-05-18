be6e670613558c13128743d436a54050353568b3
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-05-19 00:04:31 +0600

    handle search button

--- index.js
+++ index.js
@@ -29,75 +29,101 @@ async function run() {
     // post add a toy
     app.post('/toys', async (req, res) => {
       const newToy = req.body;
+      // bhul hole bad dibo
+      body.createdAt = new Date()
       console.log(newToy)
-      const result=await toyCollection.insertOne(newToy)
+      const result = await toyCollection.insertOne(newToy)
       res.send(result)
     })
 
-
+    // sorting
+    // bhul hole bad dibo start
+    const indexKeys = { price: 1 }
+    const indexOption = { name: "Price" }
+    const result = await toyCollection.createIndex(indexKeys, indexOption)
+    // bhul hole bad dibo end, niche theke sort o bad dibo
     // get method
-    app.get('/toys', async(req, res)=>{
-      const result=await toyCollection.find().toArray()
+    app.get('/toys', async (req, res) => {
+      const result = await toyCollection.find().sort({ createAt: 1 }).toArray()
       res.send(result)
     })
+
+
+
+
+
+
+    // search text
+    app.get('/getToysByName/:text', async (req, res) => {
+      const searchName = req.params.text;
+      const result = await toyCollection.find({
+        $or: [{ toyName: { $regex: searchName, $options: "i" } }]
+      }).toArray()
+      res.send(result)
+    })
+
+
+
+
+
 
 
     // get method by id
-    app.get('/toys/:id', async(req, res)=>{
-      const id=req.params.id;
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
       console.log(id)
-      const filter={_id: new ObjectId(id)}
-      const result=await toyCollection.findOne(filter)
+      const filter = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(filter)
       res.send(result)
     })
 
     // to find a specific persons shop info we need to use query params
-    app.get('/myToys', async(req, res)=>{
+    app.get('/myToys', async (req, res) => {
       // const email=req.query.email;
       // console.log(email)
-      let query={}
-      if(req.query?.email){
-        query={email: req.query.email}
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
         console.log(query)
       }
-      const result=await toyCollection.find(query).toArray();
+      const result = await toyCollection.find(query).toArray();
       res.send(result)
     })
 
 
 
     // mytoys get
-    app.get('/myToys/:id', async(req, res)=>{
-      const id=req.params.id;
+    app.get('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
       console.log(id)
-      const filter={_id: new ObjectId(id)}
-      const result=await toyCollection.findOne(filter)
+      const filter = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(filter)
       res.send(result)
     })
     // myToys update
 
-    app.put('/myToys/:id', async(req, res)=>{
-      const id=req.params.id;
-      const filter={_id: new ObjectId(id)}
+    app.put('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
       const options = { upsert: true };
-      const updatedToys=req.body;
-      const myToys={
-        $set:{
-          price:updatedToys.price,
-          quantity:updatedToys.quantity,
-          description:updatedToys.description,
+      const updatedToys = req.body;
+      const myToys = {
+        $set: {
+          price: updatedToys.price,
+          quantity: updatedToys.quantity,
+          description: updatedToys.description,
 
         }
       }
-      const result=await toyCollection.updateOne(filter, myToys, options)
+      const result = await toyCollection.updateOne(filter, myToys, options)
       res.send(result)
     })
 
     // delete toys
-    app.delete('/myToys/:id', async(req, res)=>{
-      const id=req.params.id;
-      const filter={_id: new ObjectId(id)}
-      const result=await toyCollection.deleteOne(filter)
+    app.delete('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(filter)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
